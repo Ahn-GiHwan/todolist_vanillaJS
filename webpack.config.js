59753a721782452b0e0dc6d33b4b648366b4ce7a
Ahn-GiHwan <ahngh0113@naver.com>
2021-09-24 13:50:06 +0900

    docs: webpack 설정 추가가
    HtmlWebpackPlugin
    MiniCssExtractPlugin
    CleanWebpackPlugin
    devServer setting, port 3000

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   // 시작하는 js파일
@@ -13,11 +15,51 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
 
-  // html 설정
+  module: {
+    rules: [
+      {
+        // 적용할 파일명 설정
+        test: /\.s[ac]ss$/i,
+
+        // 뒤에서 부터 적용
+        use: [
+          // Creates `style` nodes from JS strings
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+
   plugins: [
+    // html 설정
+    // html-webpack-plugin
     new HtmlWebpackPlugin({
       // 기존의 html파일을 사용
       template: "./index.html",
     }),
+
+    // css 설정
+    // mini-css-extract-plugin
+    // head안에 css를 설정하는 것이 아니라 css파일을 새로 만들어서 그 파일을 loader하게 설정
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+
+    // 기존 번들된 파일을 지우고 새로운 파일로 덮는다.
+    // clean-wepback-plugin
+    new CleanWebpackPlugin(),
   ],
+
+  // 개발 서버 설정
+  // wepback-dev-server -D
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+  },
 };

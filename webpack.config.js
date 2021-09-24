f0ae9c1b61152398182f28af7484c97671b8dada
Ahn-GiHwan <ahngh0113@naver.com>
2021-09-24 13:30:10 +0900

    docs: html 관련 설정
    html-webpack-plugin 설정

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,17 @@ module.exports = {
 
   // 번들한 파일에 대한 설정
   output: {
+    // 번들한 파일 이름
     filename: "main.js",
+    // 번들할 파일 경로
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [new HtmlWebpackPlugin()],
+
+  // html 설정
+  plugins: [
+    new HtmlWebpackPlugin({
+      // 기존의 html파일을 사용
+      template: "./index.html",
+    }),
+  ],
 };
